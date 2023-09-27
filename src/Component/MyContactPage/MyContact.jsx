3baa282b9c446e839deda81e041f3f41f055fd8a
Huzaifa246 <huzaifa24680@gmail.com>
2023-09-27 15:12:09 +0500

    Updated Range and File name

--- src/Component/MyContactPage/MyContact.jsx
+++ src/Component/MyContactPage/MyContact.jsx
@@ -1,26 +1,36 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
-import { Card, Col, Row, Dropdown, Button, Table } from 'react-bootstrap';
+import { Card, Col, Row, Dropdown, Table, Form } from 'react-bootstrap';
 import './myContact.css';
 import * as XLSX from 'xlsx';
+import { FaSearch } from 'react-icons/fa';
+import RangeSlider from "react-range-slider-input";
+import "react-range-slider-input/dist/style.css";
+// import Slider from 'react-rangeslider'
+// import 'react-rangeslider/lib/index.css'
 
 function MyContact() {
     const isSidebarOpen = useSelector((state) => state.sideBarStore.isSidebarOpen);
 
-
     const [excelData, setExcelData] = useState([]);
-    const [selectedItems, setSelectedItems] = useState([]);
-
     const [uploadTime, setUploadTime] = useState(null); // Store the upload time
+    const [searchQuery, setSearchQuery] = useState('');
 
+    const [filteredData, setFilteredData] = useState([]);
 
-    const toggleItemSelection = (item) => {
-        if (selectedItems.includes(item)) {
-            setSelectedItems(selectedItems.filter((selectedItem) => selectedItem !== item));
-        } else {
-            setSelectedItems([...selectedItems, item]);
-        }
+    const [rangeValues, setRangeValues] = useState([0, 100]);
+    const [tooltipPosition, setTooltipPosition] = useState(null);
+
+    const handleSearchInputChange = (e) => {
+        setSearchQuery(e.target.value);
     };
+    const defaultCountries = [
+        { name: 'USA', flagUrl: 'https://flagcdn.com/us.svg' },
+        { name: 'Pakistan', flagUrl: 'https://flagcdn.com/pk.svg' },
+        { name: 'United Kingdom', flagUrl: 'https://flagcdn.com/gb.svg' },
+        { name: 'Australia', flagUrl: 'https://flagcdn.com/au.svg' },
+        { name: 'China', flagUrl: 'https://flagcdn.com/cn.svg' },
+    ];
 
     const fileInputRef = useRef(null);
 
@@ -49,11 +59,16 @@ function MyContact() {
                     // Assuming your Excel sheet has columns in this order: FirstName, LastName, Phone
                     const excelData = jsonData.slice(1); // Skip the header row
 
-                    setExcelData(excelData);
-
                     // Set the upload time to the current date and time
-                    const currentTime = new Date();
-                    setUploadTime(currentTime.toLocaleString());
+                    const newExcelData = excelData?.map(() => {
+                        const currentTime = new Date();
+                        const localTime = currentTime.toLocaleString(); // Get the local date and time
+                        return { Date: currentTime, localTime }; // Add an object with Date and localTime properties
+                    });
+                    setUploadTime(newExcelData)
+
+                    setExcelData(excelData);
+                    setFilteredData(newExcelData);
                 };
 
                 reader.readAsBinaryString(file);
@@ -64,6 +79,44 @@ function MyContact() {
             }
         }
     };
+    useEffect(() => {
+        // Set the initial state when the component loads
+        setFilteredData(excelData);
+    }, [excelData]);
+
+    // When "All" button is clicked, show all data
+    const handleFilterAllClick = () => {
+        setFilteredData(excelData);
+    };
+
+    const handleFilterAgeChange = (newValues) => {
+        // Filter data based on age range
+        const filteredData = excelData?.filter((row) => {
+            const age = parseInt(row[3]); // Assuming age is at index 3 in your data
+            return age >= newValues[0] && age <= newValues[1];
+        });
+
+        setFilteredData(filteredData);
+    };
+
+
+    const handleFilterGenderChange = (selectedGender) => {
+        // Filter data based on selected gender
+        const filteredData = excelData.filter((row) => {
+            return row[4] === selectedGender; // Assuming gender is at index 4 in your data
+        });
+
+        setFilteredData(filteredData);
+    };
+
+    const handleFilterCountryChange = (selectedCountry) => {
+        // Filter data based on selected country
+        const filteredData = excelData.filter((row) => {
+            return row[5] === defaultCountries[selectedCountry].name; // Assuming country is at index 5 in your data
+        });
+
+        setFilteredData(filteredData);
+    };
 
     console.log(excelData, 'asad');
     return (
@@ -72,18 +125,102 @@ function MyContact() {
                 <div style={{ marginTop: '6rem' }}></div>
                 <Row style={{ marginBottom: '20px', marginLeft: '10px', width: '99%' }}>
                     <Col>
-                        <Card className='card-box-border border-shadow-style'>
-                            <div className='card-drop-style'>
-                                <h2 style={{ padding: '10px', paddingTop: '20px', fontWeight: '600' }}>
-                                    My Contacts
-                                </h2>
-                            </div>
-                            <Card className='card-box-border' style={{ padding: '15px 0' }}>
+                        <div className='card-drop-style'>
+                            <h1 style={{ padding: '10px', paddingTop: '20px', fontWeight: '600' }}>
+                                My Contacts
+                            </h1>
+                        </div>
+                        <Card className='card-box-border my-Context-style'>
+                            <Card className='card-box-border'>
                                 <form>
                                     <Row>
-                                        <Col xs={12} md={12} lg={12}>
+                                        <Col>
                                             <div className='My-form-input'>
-                                                <span>
+                                                <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
+                                                    <button
+                                                        type='button'
+                                                        className={filteredData === excelData ? 'selected-btn' : 'unselected-dropdown'}
+                                                        onClick={handleFilterAllClick}
+                                                    >
+                                                        All
+                                                    </button>
+                                                    <Dropdown>
+                                                        <Dropdown.Toggle
+                                                            className={filteredData === 'Age' ? 'selected-btn' : 'unselected-dropdown'}
+                                                            id="filterDropdown"
+                                                        >
+                                                            Age
+                                                        </Dropdown.Toggle>
+                                                        <Dropdown.Menu>
+                                                            <RangeSlider
+                                                                min={0}
+                                                                max={100}
+                                                                step={1}
+                                                                rangeValues={rangeValues}
+                                                                id="range-slider-gradient"
+                                                                className="margin-lg"
+                                                                onChange={(newValues) => {
+                                                                    setRangeValues(newValues);
+                                                                    handleFilterAgeChange(newValues);
+                                                                }}
+                                                            />
+
+                                                            <div className='range-values'>
+                                                                <span>{rangeValues[0]}</span> - <span>{rangeValues[1]}</span>
+                                                            </div>
+
+
+                                                        </Dropdown.Menu>
+                                                    </Dropdown>
+                                                    <Dropdown>
+                                                        <Dropdown.Toggle
+                                                            className={filteredData === 'Gender' ? 'selected-btn' : 'unselected-dropdown'}
+                                                            id="filterDropdown"
+                                                        >
+                                                            Gender
+                                                        </Dropdown.Toggle>
+                                                        <Dropdown.Menu>
+                                                            <Dropdown.Item value="Male"
+                                                                onClick={() => handleFilterGenderChange("Male")}
+                                                            >Male</Dropdown.Item>
+                                                            <Dropdown.Item value="Female"
+                                                                onClick={() => handleFilterGenderChange("Female")}
+                                                            >Female</Dropdown.Item>
+                                                            <Dropdown.Item value="Others"
+                                                                onClick={() => handleFilterGenderChange("Others")}
+                                                            >Others</Dropdown.Item>
+                                                        </Dropdown.Menu>
+                                                    </Dropdown>
+                                                    <Dropdown>
+                                                        <Dropdown.Toggle
+                                                            className={filteredData === 'Country' ? 'selected-btn' : 'unselected-dropdown'}
+                                                            id="filterDropdown"
+                                                        >
+                                                            Country
+                                                        </Dropdown.Toggle>
+                                                        <Dropdown.Menu>
+                                                            {defaultCountries.map((country, index) => (
+                                                                <Dropdown.Item key={index} value={index}
+                                                                    onClick={() => handleFilterCountryChange(index)}
+                                                                >
+                                                                    <img src={country.flagUrl} className="country-flag" style={{ height: "1.5vh", paddingRight: "5px" }} />
+                                                                    {country.name}
+                                                                </Dropdown.Item>
+                                                            ))}
+                                                        </Dropdown.Menu>
+                                                    </Dropdown>
+                                                </div>
+                                                <div style={{ display: 'flex' }}>
+                                                    <div className="search-container">
+                                                        <FaSearch className="search-icon" />
+                                                        <input
+                                                            type="text"
+                                                            placeholder="Search..."
+                                                            value={searchQuery}
+                                                            onChange={handleSearchInputChange}
+                                                            className='input-search'
+                                                        />
+                                                    </div>
                                                     <button
                                                         type='button'
                                                         onClick={handleFileSelect}
@@ -98,7 +235,7 @@ function MyContact() {
                                                         onChange={handleFileInputChange}
                                                         style={{ display: 'none' }} // Hide the input
                                                     />
-                                                </span>
+                                                </div>
                                             </div>
                                         </Col>
                                     </Row>
@@ -109,46 +246,53 @@ function MyContact() {
                 </Row>
                 <Row style={{ marginBottom: '20px', marginLeft: '10px', width: '99%' }}>
                     <Col>
-                        <Card className='card-box-border border-shadow-style' style={{ padding: '15px' }}>
-                            <h4>
-                                Contact List
-                            </h4>
-                            <Table striped className='main-table'>
+                        {/* {filteredData?.length === 0 ? (
+                            <>
+                                <p style={{ textAlign: 'center', fontWeight: 'bold' }}>No Data Found!!!</p>
+                            </>
+                        ) : ( */}
+                        <Card className='card-box-border border-shadow-style style-myContactCard'>
+                            <Table style={{ marginBottom: "0" }}>
                                 <thead>
-                                    <tr>
-                                        <th>S.No</th>
-                                        <th style={{ display: 'flex', justifyContent: 'center' }}>Date&Time</th>
-                                        <th style={{ paddingLeft: '10px' }}>FirstName</th>
-                                        <th style={{ paddingLeft: '10px' }}>LastName</th>
-                                        <th style={{ paddingLeft: '10px' }}>Email</th>
-                                        <th style={{ paddingLeft: '10px' }}>Age</th>
-                                        <th style={{ paddingLeft: '10px' }}>Gender</th>
-                                        <th style={{ paddingLeft: '10px' }}>Country</th>
-                                        <th style={{ paddingLeft: '10px' }}>Phone</th>
-                                        <th style={{ paddingLeft: '10px' }}>Description</th>
+                                    <tr style={{ color: "#888" }}>
+                                        <th style={{ width: "5%" }}>S.No</th>
+                                        <th style={{ width: "15%", textAlign: 'center' }}>Date & Time</th>
+                                        <th style={{ width: "10%", paddingLeft: '10px' }}>FirstName</th>
+                                        <th style={{ width: "10%", paddingLeft: '10px' }}>LastName</th>
+                                        <th style={{ width: "10%", paddingLeft: '10px' }}>Email</th>
+                                        <th style={{ width: "5%", paddingLeft: '10px' }}>Age</th>
+                                        <th style={{ width: "10%", paddingLeft: '10px' }}>Gender</th>
+                                        <th style={{ width: "10%", paddingLeft: '10px' }}>Country</th>
+                                        <th style={{ width: "10%", paddingLeft: '10px' }}>Phone</th>
+                                        <th style={{ width: "15%", paddingLeft: '10px' }}>Description</th>
                                     </tr>
                                 </thead>
-                                <tbody>
-                                    {excelData?.map((row, index) => (
-                                        <tr key={index}>
-                                            <td>{index + 1}</td> 
-                                            <td style={{textAlign: 'center'}}>{uploadTime}</td>
-                                            <td>{row[0]}</td> 
-                                            <td>{row[1]}</td> 
-                                            <td>{row[2]}</td>
-                                            <td>{row[3]}</td>
-                                            <td>{row[4]}</td>
-                                            <td>{row[5]}</td>
-                                            <td>{row[6]}</td>
-                                            <td>{row[7]}</td>
-                                        </tr>
-                                    ))}
-                                </tbody>
                             </Table>
                         </Card>
+                        {filteredData?.map((row, index) => (
+                            <Card className='card-box-border border-shadow-style tbody-style-card'>
+                                <Table style={{ marginBottom: "0" }}>
+                                    <tbody>
+                                        <tr key={index}>
+                                            <td style={{ width: "4%", textAlign: 'center' }}>{index + 1}</td>
+                                            <td style={{ width: "15%", textAlign: 'center' }}>{uploadTime[index]?.Date?.toLocaleString()}</td>
+                                            <td style={{ width: "10%", textAlign: 'center' }}>{row[0]}</td>
+                                            <td style={{ width: "10%", textAlign: 'center' }}>{row[1]}</td>
+                                            <td style={{ width: "10%" }}>{row[2]}</td>
+                                            <td style={{ width: "5%" }}>{row[3]}</td>
+                                            <td style={{ width: "10%" }}>{row[4]}</td>
+                                            <td style={{ width: "10%" }}>{row[5]}</td>
+                                            <td style={{ width: "10%" }}>{row[6]}</td>
+                                            <td style={{ width: "15%" }}>{row[7]}</td>
+                                        </tr>
+                                    </tbody>
+                                </Table>
+                            </Card>
+                        ))}
+                        {/* )} */}
                     </Col>
-                </Row>
-            </div>
+                </Row >
+            </div >
         </>
     );
 }
